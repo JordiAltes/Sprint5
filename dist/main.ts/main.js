2e2414b83f6c4ea5c53a0ddccd7c3fc41c43adda
JordiAltes <jordalan8@gmail.com>
2023-01-31 12:46:00 +0100

    exercici3 casi acabat

--- dist/main.ts/main.js
+++ dist/main.ts/main.js
@@ -1,12 +1,25 @@
 "use strict";
-const API_URL = 'https://icanhazdadjoke.com/';
-let HTMLResponse = document.querySelector('#joke');
+const API_URL = "https://icanhazdadjoke.com/";
+let HTMLResponse = document.querySelector("#joke");
+let joke;
 function nextJoke() {
-    fetch(`${API_URL}/`, { headers: { Accept: 'application/json' } })
+    const scoreButton = document.getElementById("scoreButtons");
+    scoreButton.style.display = "";
+    fetch(`${API_URL}/`, { headers: { Accept: "application/json" } })
         .then((response) => response.json())
         .then((data) => {
-        const joke = data.joke;
+        joke = data.joke;
         console.log(joke);
         HTMLResponse.innerHTML = joke;
     });
+}
+const arrayJokes = [];
+function scoreJoke(id) {
+    const scoreJoke = id;
+    const date = new Date();
+    const dateToString = date.toISOString();
+    const objectJoke = { joke: joke, date: dateToString, score: scoreJoke };
+    console.log(objectJoke);
+    arrayJokes.push(objectJoke);
+    console.log(arrayJokes);
 }
--- main.ts
+++ main.ts
@@ -1,16 +1,30 @@
-const API_URL = 'https://icanhazdadjoke.com/';
+const API_URL = "https://icanhazdadjoke.com/";
 
-let HTMLResponse:any = document.querySelector('#joke');
+let HTMLResponse: any = document.querySelector("#joke");
+let joke:string;
+ function nextJoke() {
+ 
+  const scoreButton = document.getElementById(
+    "scoreButtons"
+  ) as HTMLButtonElement;
 
-function nextJoke() {
-  fetch(`${API_URL}/`, { headers: { Accept: 'application/json' } })
+  scoreButton.style.display = "";
+
+   fetch(`${API_URL}/`, { headers: { Accept: "application/json" } })
     .then((response) => response.json())
     .then((data) => {
-      const joke:string = data.joke;
+      joke = data.joke;
       console.log(joke);
       HTMLResponse.innerHTML = joke;
-      
-      
-});
+    });
 }
-
+const arrayJokes:any = []
+function scoreJoke(id:number) {
+  const scoreJoke = id;
+  const date = new Date();
+  const dateToString = date.toISOString();
+  const objectJoke = {joke: joke, date: dateToString, score: scoreJoke}
+  console.log(objectJoke);
+  arrayJokes.push(objectJoke)
+  console.log(arrayJokes);
+}
