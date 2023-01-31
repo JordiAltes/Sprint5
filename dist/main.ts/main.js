ac15263ba952ba88a9ce6e8f90c08534b9757174
JordiAltes <jordalan8@gmail.com>
2023-01-31 10:04:19 +0100

    exercici2 acabat

--- dist/main.ts/main.js
+++ dist/main.ts/main.js
@@ -1,12 +1,14 @@
 "use strict";
 const API_URL = 'https://icanhazdadjoke.com/';
-let HTMLResponse = document.querySelector('.result');
+let HTMLResponse = document.querySelector('#joke');
 function nextJoke() {
     fetch(`${API_URL}/`, { headers: { Accept: 'application/json' } })
         .then((response) => response.json())
         .then((data) => {
         const joke = data.joke;
         console.log(joke);
-        HTMLResponse.innerHTML = joke;
+        if (HTMLResponse != null) {
+            HTMLResponse.innerHTML = joke;
+        }
     });
 }
--- main.ts
+++ main.ts
@@ -1,14 +1,16 @@
 const API_URL = 'https://icanhazdadjoke.com/';
 
-let HTMLResponse:any = document.querySelector('.result');
+let HTMLResponse:any = document.querySelector('#joke');
 
 function nextJoke() {
   fetch(`${API_URL}/`, { headers: { Accept: 'application/json' } })
     .then((response) => response.json())
     .then((data) => {
-      const joke = data.joke;
+      const joke:string = data.joke;
       console.log(joke);
-    HTMLResponse.innerHTML = joke;
+      if (HTMLResponse != null) {
+        HTMLResponse.innerHTML = joke;
+      }
 });
 }
 
